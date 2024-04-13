db1544a3367b64714bbe62b7688932216796ddc8
Hasina-Tiana <hasinatianarah@gmail.com>
2024-04-13 07:32:10 +0300

    dernière modification

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
           <h1 className="text-8xl text-[#6B4D97]">Let&#39;s be friend !</h1>
           <p className="text-justify text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit maxime ullam officia reiciendis iusto, laboriosam perferendis mollitia accusantium ipsum! Repellendus accusamus veritatis non asperiores dignissimos quo maiores voluptas inventore dicta.</p>
           <div className="flex justify-center space-x-20 *:text-xl">
-            <p className="bg-[#6B4D97] text-white px-4 py-2 rounded">Explore</p>
-            <p className="text-[#6B4D97] px-4 py-2 rounded shadow shadow-slate-500">Learn more</p>
+            <button className="bg-[#6B4D97] text-white px-4 py-2 rounded">Explore</button>
+            <button className="text-[#6B4D97] px-4 py-2 rounded shadow shadow-slate-500">Learn more</button>
           </div>
         </div>
         <div className="w-1/2 flex justify-center items-center">
