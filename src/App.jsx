6c1d38aa8cd780983bca816e7b0521965bea46d2
Hasina-Tiana <hasinatianarah@gmail.com>
2024-04-13 07:38:26 +0300

    modification UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Navbar/>
-      <div className="w-full px-8 flex items-center justify-center mt-10">
+      <div className="w-full px-10 flex items-center justify-center mt-10">
         <div className="w-1/2 space-y-12">
           <h1 className="text-8xl text-[#6B4D97]">Let&#39;s be friend !</h1>
           <p className="text-justify text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit maxime ullam officia reiciendis iusto, laboriosam perferendis mollitia accusantium ipsum! Repellendus accusamus veritatis non asperiores dignissimos quo maiores voluptas inventore dicta.</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 const Navbar = () => {
     return (
-        <div className="flex h-[56px] items-center justify-between px-40 *:text-xl mt-2">
+        <div className="flex h-[56px] items-center justify-between px-32 *:text-xl mt-2">
             <div>
                 <button className="text-[#6B4D97] text-lg shadow shadow-slate-500 px-4 py-2 rounded">FRIENDS</button>
             </div>
             <div className="flex justify-between mx-4 items-center space-x-6">
-                <p>About</p>
-                <p>Person</p>
+                <a href="#">About</a>
+                <a href="#">Person</a>
                 <button className="text-white bg-[#ACCCEA] px-4 py-2 rounded shadow shadow-slate-300">Contact</button>
             </div>
         </div>
